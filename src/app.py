2dc6f6244376bf78b96f7e68fde6c661a5803b42
Shorn <shorn.keld@gnosis.io>
2024-02-25 17:24:49 +0100

    tidy up and hide amount input

--- src/app.py
+++ src/app.py
@@ -1,75 +1,52 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 import os
-from pathfinder import Pathfinder
+import subprocess
+import json
 from dotenv import load_dotenv
+from pathfinder import Pathfinder
+from streamlit_d3graph import d3graph
 
-# Load environment variables
+# Load environment variables and initialize Pathfinder
 load_dotenv()
-
-# Initialize Pathfinder
 pathfinder = Pathfinder()
 
-# Set page to wide layout
+# Set page config
 st.set_page_config(layout="wide")
-
-# Set the title of the app
 st.title('Circles Pathfinder Viewer')
 
-# Navigation in the sidebar
-with st.sidebar:
-    st.header("Navigation")
-    # Simple navigation link - for now, it's just a static display since there's only one page
-    st.markdown("Path Viewer")
-
-
-# Use Streamlit's sidebar for input fields
-with st.sidebar:
-    st.header("Input Parameters")
-    # Circles address or username inputs
-    user_source = st.text_input("From (Username or Address)", value=os.getenv('SOURCE_ADDRESS', ''))
-    user_sink = st.text_input("To (Username or Address)", value=os.getenv('SINK_ADDRESS', ''))
-    amount = st.text_input("Amount", value=os.getenv('AMOUNT'))
-    # Button to trigger Sankey diagram generation
-    generate_button = st.button('Generate Sankey Data')
-
-# Function to resolve input to address
 def resolve_input_to_address(user_input):
     if user_input.startswith('0x'):
-        return user_input  # Assume it's an address
-    elif user_input.strip() != "":  # Check if input is not just whitespace
-        return pathfinder.resolve_username_to_address(user_input)  # Assume it's a username
+        return user_input
+    elif user_input.strip() != "":
+        return pathfinder.resolve_username_to_address(user_input)
     else:
         return None
 
-# Main page layout for displaying the chart
-if generate_button:
-    # Resolve source and sink inputs to addresses
-    source_address = resolve_input_to_address(user_source)
-    if not source_address:
-        st.error(f"Could not resolve source: {user_source}")
-        st.stop()
+# Sidebar navigation and form inputs
+with st.sidebar:
+    selected = option_menu("Main Menu", ["Path Viewer", "Explore My Trust Graph"],
+                           icons=["graph-up", "compass"], menu_icon="cast", default_index=0)
 
-    sink_address = resolve_input_to_address(user_sink)
-    if not sink_address:
-        st.error(f"Could not resolve sink: {user_sink}")
-        st.stop()
+    if selected == "Path Viewer":
+        st.header("Path Viewer Inputs")
+        user_source = st.text_input("From (Username or address)", value="shorn", key="pv_user_source")
+        user_sink = st.text_input("To (Username or address)", value="Martin", key="pv_user_sink")
+        amount ="999999999999999999999999999"
+        generate_chart_button = st.button('Generate Chart', key='generate_chart')
 
-    # Validate and convert amount to integer
-    try:
-        amount_int = int(amount)
-    except ValueError:
-        st.error("Please enter a valid amount.")
-        st.stop()
+# Path Viewer section logic
+if selected == "Path Viewer" and generate_chart_button:
+    with st.spinner('Generating chart... Please wait'):
+        source_address = resolve_input_to_address(user_source)
+        sink_address = resolve_input_to_address(user_sink)
 
-    # Use the get_args_for_path method to start with
-    token_owner, srcs, dests, wads, capacity = pathfinder.get_args_for_path(source_address, sink_address, amount_int)
-    if not token_owner:
-        st.error("Failed to generate path. Please check the inputs.")
-        st.stop()
-
-    # Use the get_shanky method to generate the data needed for the Sankey diagram
-    source_, target_, value_, flow_labels, labels = pathfinder.get_shanky(token_owner, srcs, dests, wads)
-    
-    # Generate and display a visual Sankey diagram
-    fig = pathfinder.draw_shanky(source_, target_, value_, flow_labels, labels)
-    st.plotly_chart(fig, use_container_width=True)  # Display the figure in the Streamlit app, using the full width of the container
+        if source_address and sink_address:
+            token_owner, srcs, dests, wads, capacity = pathfinder.get_args_for_path(source_address, sink_address, amount)
+            if token_owner:
+                fig = pathfinder.draw_shanky(*pathfinder.get_shanky(token_owner, srcs, dests, wads))
+                st.plotly_chart(fig, use_container_width=True)
+            else:
+                st.error("Failed to generate path. Please check the inputs.")
+        else:
+            st.error("Invalid source or sink address.")
